a0d7ea2e20553f53decf6147086423f3f227857f
Will O'Brien <will.obrien@dibit.co>
2013-01-13 14:46:07 -0500

    Added blockstep cookbook config & knife path to it

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -5,5 +5,6 @@ client_key               'will.pem'
 validation_client_name   'chef-validator'
 validation_key           'validation.pem'
 chef_server_url          'http://chef.blockstep.com:4000'
+cookbook_path            '../cookbooks'
 cache_type               'BasicFile'
 cache_options( :path => 'checksums' )
--- cookbooks/blockstep/metadata.rb
+++ cookbooks/blockstep/metadata.rb
@@ -1,7 +1,9 @@
+name             "blockstep"
 maintainer       "Will O'Brien"
 maintainer_email "Will.obrien@dibit.co"
 license          "All rights reserved"
 description      "Installs/Configures blockstep"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-depends "application_nginx"
 version          "0.1.0"
+
+depends "application_nginx"
