65d6ee8d14fe25cc8dd87bc982cfd08d1764dc7a
Will O'Brien <will.obrien@dibit.co>
2013-01-27 15:36:47 -0500

    Working rvm install

--- cookbooks/blockstep/recipes/default.rb
+++ cookbooks/blockstep/recipes/default.rb
@@ -9,6 +9,28 @@
 
 package 'git'
 
+service "blockstep" do
+  provider Chef::Provider::Service::Upstart
+  supports :restart => true, :stop => true, :start => true
+  action :nothing
+end
+
+rvm_shell 'procfile foreman export' do
+  action :nothing
+  ruby_string '1.9.3'
+
+  current_path = '/opt/blockstep/current'
+
+  foreman_options = ["HOME=/root foreman export upstart /etc/init"]
+  foreman_options << "-a blockstep"
+  foreman_options << "-u will"
+  foreman_options << "-f Procfile"
+
+  code %{ cd #{current_path} && #{foreman_options.join(" ")} }
+
+end
+
+
 unicorn_config "/etc/unicorn/blockstep.rb" do
   listen 80 => {:tcp_nodelay => true, :backlog => 100 }
   working_directory "/opt/blockstep/current"
@@ -39,5 +61,21 @@ deploy "blockstep" do
   deploy_to "/opt/blockstep"
   action :deploy
   ssh_wrapper "/tmp/keyz/ssh-wrapper.sh"
+
+
+  before_symlink do
+    rvm_shell 'procfile bundle install' do
+      ruby_string '1.9.3'
+      user 'will'
+      cwd release_path
+
+      code %{
+        bundle install --deployment --without development,test
+      }
+    end
+  end
+
 end
+
+
 
