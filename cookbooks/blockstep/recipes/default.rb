5b2e11d16e820d9ea289864bbd2e2fd3998f412c
Will O'Brien <will.obrien@dibit.co>
2013-02-04 21:44:03 -0500

    Set up unicorn correctly

--- cookbooks/blockstep/recipes/default.rb
+++ cookbooks/blockstep/recipes/default.rb
@@ -18,16 +18,27 @@ end
 rvm_shell 'procfile foreman export' do
   action :nothing
   ruby_string '1.9.3'
+  user 'root'
 
   current_path = '/opt/blockstep/current'
 
-  foreman_options = ["HOME=/root foreman export upstart /etc/init"]
+  foreman_options = ["foreman export upstart /etc/init"]
   foreman_options << "-a blockstep"
   foreman_options << "-u will"
+  foreman_options << "-l /opt/blockstep/current/log"
   foreman_options << "-f Procfile"
+  foreman_options << "-t /opt/blockstep/shared/templates"
 
-  code %{ cd #{current_path} && #{foreman_options.join(" ")} }
+  code %{ cd #{current_path} && HOME=/root bundle exec #{foreman_options.join(" ")} }
 
+end
+rvm_shell 'rails3 asset pipeline' do
+  action :nothing
+  ruby_string '1.9.3'
+  user 'will'
+
+  current_path = '/opt/blockstep/current'
+  code %{ cd #{current_path} && bundle exec rake assets:precompile }
 end
 
 
@@ -39,6 +50,25 @@ end
 directory "/tmp/keyz/.ssh" do
   owner "will"
   recursive true
+end
+
+file "/var/log/blockstep" do
+  action :create_if_missing
+  owner "root"
+  mode "0755"
+end
+
+directory "/opt/blockstep/shared/templates" do
+  owner "will"
+  group "will"
+  mode "770"
+end
+
+%w{master.conf.erb process.conf.erb process_master.conf.erb}.each do |f|
+  template "/opt/blockstep/shared/templates/#{f}" do
+    source "foreman/#{f}"
+    variables :path => "/opt/blockstep/current", :ruby_version => "1.9.3"
+  end
 end
 
 cookbook_file "/tmp/keyz/.ssh/id_deploy" do
@@ -53,7 +83,9 @@ cookbook_file "/tmp/keyz/ssh-wrapper.sh" do
   mode 0700
 end
 
+#deploy_revision "blockstep" do
 deploy "blockstep" do
+
   repo "git@github.com:will-ob/com.blockstep.git"
   revision "master"
   environment "development"
@@ -70,10 +102,16 @@ deploy "blockstep" do
       cwd release_path
 
       code %{
-        bundle install --deployment --without development,test
+        bundle install --deployment --without development test
       }
     end
   end
+
+  notifies :run,     resources("rvm_shell[rails3 asset pipeline]"), :immediate
+  notifies :run,     resources("rvm_shell[procfile foreman export]"), :immediate
+
+  notifies :enable,  resources("service[blockstep]"), :delayed
+  notifies :restart, resources("service[blockstep]"), :delayed
 
 end
 
